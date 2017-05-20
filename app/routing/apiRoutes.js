6be07df4f9b671f43c52412a94c23501077bbe27
dblanchard13 <davidb.prof@gmail.com>
2017-05-19 21:32:57 -0700

    Homework comments

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,7 @@
-var express = require('express');
+// var express = require('express');
 var path = require('path');
-var app = express();
+// var app = express();
+// You don't need to instantiate another express app since you're passing it into the function exported by module.exports.
 
 var friendsInfo	= require('../data/friends.js');
 
@@ -20,10 +21,13 @@ module.exports = function(app){
 	});
 
 	app.post('/api/friends', function(req, res) {
+		// by defining this here, you're shadowing the outer `mathFriends` function.
+		// Also, it's alittle misleading, since `req.body` is actually the user's survey response.
 		var matchFriends = req.body;
 		friendsInfo.push(matchFriends);
 		res.json(friendsInfo);
-		checkScores();
+		// you're not defining `checkScores` anywhere
+		// checkScores();
 	});
 
 	app.post('/api/clear', function(req, res) {
